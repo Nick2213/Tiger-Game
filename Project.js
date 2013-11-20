aa7c61d07ccb104b050d89041a590f2e5ebffa69
DominicRossillo <domrossillo@Optonline.net>
2013-11-20 18:45:34 -0500

    change in class to finish the in start page and start of the character screen

--- Project.js
+++ Project.js
@@ -2,6 +2,18 @@
 
 
 function startgame() {
-document.getElementById('StartSceen').style.visibility="hidden";
+  document.getElementById('StartScreen').style.display='none';
+  document.getElementById('CharCreationScreen').style.display='block';
+}
+
+function charcreation() {
+document.getElementById("CharCreationScreen").style.visibility="show";
+}
+
+function maletiger() {
+
+}
+
+function femaletiger() {
 
 }
