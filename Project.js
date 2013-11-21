c0002e0137bab59759a2f7668c193f73c3ce322e
Nick Titolo <Nick@148-100-154-35.foxnet.marist.edu>
2013-11-20 22:12:00 -0500

    updated project.

--- Project.js
+++ Project.js
@@ -9,11 +9,24 @@ function startgame() {
 function charcreation() {
 document.getElementById("CharCreationScreen").style.visibility="show";
 }
-
+//gender info
+gender=0
 function maletiger() {
+gender=1
 
 }
 
 function femaletiger() {
+gender=2
 
 }
+function SubmitChar(){ 
+	document.getElementById("namebox").value ;
+		if(gender===0)
+		{
+		alert("You forgot to chose a gender");
+		}
+		if(document.getElementById("namebox").value ==='')
+		alert("You forgot to make a name")
+}
+
